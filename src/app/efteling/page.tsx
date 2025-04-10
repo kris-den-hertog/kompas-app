9d53f65ca1fc2bfd154f3db0a95455196e053d81
Kris den Hertog <krisdenhertog@icloud.com>
2025-04-10 13:56:46 +0200

    UI fixed

--- src/app/efteling/page.tsx
+++ src/app/efteling/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
     );
 
     return (
-        <main className="container mx-auto py-8 bg-main-100">
+        <main className="container mx-auto min-h-[100vh] min-w-[100vw] py-8 bg-main-100">
             <div className='w-full flex flex-col items-center mb-10'>
                 <div className='w-[350px] h-[155px] bg-main-300 rounded-[16px] mb-[55px] flex flex-col items-center justify-center'>
                     <h1 className='text-[48px] text-main-500 m-0'>Efteling</h1>
@@ -97,7 +97,25 @@ export default function Home() {
                     height={50}
                     alt="Decorative divider"
                 />
+                {/* Search Bar */}
+
+                <div className="relative mt-10">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none ">
+                        <svg className="w-4 h-4 text-main-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input
+                        type="search"
+                        className="block w-[350px] p-4 pl-10 text-sm rounded-[17px] bg-main-200 border-[1px] border-main-400 focus:ring-main-500 focus:border-main-500"
+                        placeholder="Search attractions..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </div>
             </div>
+
+
 
             {loading && <p className="text-lg">Loading attraction data...</p>}
             {error && (
@@ -115,35 +133,19 @@ export default function Home() {
             {!loading && !error && filteredAttractions.length > 0 && (
                 <>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {/* Search Bar */}
-                        <div className="w-full flex justify-center max-w-md mb-8 ">
-                            <div className="relative">
-                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none ">
-                                    <svg className="w-4 h-4 text-main-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                    </svg>
-                                </div>
-                                <input
-                                    type="search"
-                                    className="block w-[350px] p-4 pl-10 text-sm rounded-[17px] bg-main-200 border-[1px] border-main-400 focus:ring-main-500 focus:border-main-500"
-                                    placeholder="Search attractions..."
-                                    value={searchTerm}
-                                    onChange={(e) => setSearchTerm(e.target.value)}
-                                />
-                            </div>
-                        </div>
+
                         {filteredAttractions.map((attraction) => (
-                            <div key={attraction.id} className="border rounded-lg p-4 shadow-sm">
+                            <div key={attraction.id} className="p-4 shadow-sm">
                                 <h2 className="text-xl font-semibold mb-2">{attraction.name}</h2>
 
                                 <div className="flex justify-between mb-2">
                                     <span className="font-medium">Status:</span>
                                     <span
                                         className={`px-2 py-1 rounded text-sm font-medium ${attraction.status === "OPERATING"
-                                                ? "bg-green-100 text-green-800"
-                                                : attraction.status === "DOWN"
-                                                    ? "bg-red-100 text-red-800"
-                                                    : "bg-gray-100 text-gray-800"
+                                            ? "bg-green-100 text-green-800"
+                                            : attraction.status === "DOWN"
+                                                ? "bg-red-100 text-red-800"
+                                                : "bg-gray-100 text-gray-800"
                                             }`}
                                     >
                                         {attraction.status}
@@ -154,16 +156,16 @@ export default function Home() {
                                     <span className="font-medium">Wait Time:</span>
                                     <span
                                         className={`font-medium ${attraction.status === "OPERATING" &&
+                                            attraction.queue?.STANDBY?.waitTime !== undefined &&
+                                            attraction.queue.STANDBY.waitTime !== null &&
+                                            attraction.queue.STANDBY.waitTime > 40
+                                            ? "text-red-600"
+                                            : attraction.status === "OPERATING" &&
                                                 attraction.queue?.STANDBY?.waitTime !== undefined &&
                                                 attraction.queue.STANDBY.waitTime !== null &&
-                                                attraction.queue.STANDBY.waitTime > 40
-                                                ? "text-red-600"
-                                                : attraction.status === "OPERATING" &&
-                                                    attraction.queue?.STANDBY?.waitTime !== undefined &&
-                                                    attraction.queue.STANDBY.waitTime !== null &&
-                                                    attraction.queue.STANDBY.waitTime > 20
-                                                    ? "text-yellow-600"
-                                                    : ""
+                                                attraction.queue.STANDBY.waitTime > 20
+                                                ? "text-yellow-600"
+                                                : ""
                                             }`}
                                     >
                                         {formatWaitTime(attraction)}
